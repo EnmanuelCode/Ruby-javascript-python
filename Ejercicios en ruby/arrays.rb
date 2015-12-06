46d65261096bd2a96abc5fd0bf25f99873fae1c2
EnmanuelCode <enmanuelmacc@gmail.com>
2015-12-05 21:17:04 -0500

    arrays
    arrays simple

---
+++ Ejercicios en ruby/arrays.rb
@@ -0,0 +1,28 @@
+ 
+ puts "arrays"
+
+ class Programa
+
+ 	def initialize()
+ 	end
+ 	def arrays()
+     
+    
+   arrayNum = [1,2,3,4,5]
+   
+   
+   arrayNum.each do |x|
+   
+   x += 30
+   puts "#{x}"
+  
+   
+   end
+   
+
+end
+
+end
+objeto = Programa.new()
+objeto.arrays
+gets()
