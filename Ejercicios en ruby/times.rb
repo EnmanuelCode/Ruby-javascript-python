855e70cb774ce97339c9fbef0a3aee5dc9f941b5
EnmanuelCode <enmanuelmacc@gmail.com>
2015-12-05 21:17:36 -0500

    times
    iterator

---
+++ Ejercicios en ruby/times.rb
@@ -0,0 +1,20 @@
+ 
+ puts "iterador Times"
+
+ class Programa
+
+ 	def initialize()
+ 	end
+ 	def times()
+    
+  
+    50.times {puts "no hice la tarea"}
+    
+   
+
+end
+
+end
+objeto = Programa.new()
+objeto.times
+gets()
